82a2ccbbe5c7a089668706a1fb09592807b39744
SulimAnxO1 <mxsulaimansufian@gmail.com>
2025-09-25 21:28:26 +0600

    linked js & css to html file

---
+++ JavaScript/dom.js
@@ -0,0 +1,6 @@
+//DOM - Document Object Model
+// DOM objects are called nodes
+console.log(window);
+console.log(window.document);
+console.dir(document); // console.dir(window.document); alternet way to print document
+// dir is used to print objects it is also a part of windows object
